04524455182429ef16555945889df3719a5d69c6
Rudy Bermúdez <hello@rudybermudez.io>
2018-04-26 18:00:04 -0700

    Populate user table

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
 $(document).ready(function(){
     // Code won't execute until the document is loaded
     loadUsers();
-    todosFor("1");
-    albumsFor("1");
+    todosFor(1);
+    albumsFor(1);
 });
 
 /*
@@ -14,13 +14,26 @@ $(document).ready(function(){
 function loadUsers() {
     $.getJSON( "https://jsonplaceholder.typicode.com/users")
         .done(function( data ) {
+            var table = $(document.getElementsByClassName("userTable"));
+            console.log(table);
             $.each( data, function( i, item ) {
                 console.log(item["id"], item["name"], item["email"], item["company"]["name"]);
+                let row = $('<tr>').addClass('row');
+                let todosBtn = $('<button>').addClass("todoButton").text("View Todos");
+                let albumsBtn = $('<button>').addClass("albumButton").text("View Albums");
+                row.append($("<td>").text(item["name"]));
+                row.append($("<td>").text(item["email"]));
+                row.append($("<td>").text(item["company"]["name"]));
+                row.append(todosBtn);
+                row.append(albumsBtn);
+                table.append(row)
             })
     });
 }
 
+
 function todosFor(userID) {
+    userID = userID.toString();
     $.getJSON( `https://jsonplaceholder.typicode.com/todos?userId=${userID}`)
         .done(function( data ) {
             $.each( data, function( i, item ) {
@@ -30,6 +43,7 @@ function todosFor(userID) {
 }
 
 function albumsFor(userID) {
+    userID = userID.toString();
     $.getJSON( `https://jsonplaceholder.typicode.com/albums?userId=${userID}`)
         .done(function( data ) {
             $.each( data, function( i, item ) {
