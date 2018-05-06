36f7aefb82522097cd6099d8c53823f03e72361e
Rudy Bermúdez <hello@rudybermudez.io>
2018-05-06 13:16:01 -0700

    Initial Commit

---
+++ index.js
@@ -0,0 +1,49 @@
+"use strict"
+
+
+function toggleBackgroundPinkOrange() {
+    let buttonText = document.getElementById("backgroundToggleButton").innerHTML;
+    if (buttonText==="Click for pink!") {
+        document.getElementById("two").style.backgroundColor = "pink";
+        document.getElementById("backgroundToggleButton").innerHTML = "Click for orange!"
+    } else {
+        document.getElementById("two").style.backgroundColor = "orange";
+        document.getElementById("backgroundToggleButton").innerHTML = "Click for pink!"
+    }
+}
+
+function checkForReturnKey() {
+    var input = document.getElementById("userInputField")
+
+    input.addEventListener("keyup", function (event) {
+        event.preventDefault();
+        // Number 13 is the "Enter" key on the keyboard
+        if (event.keyCode === 13) {
+            // Trigger the button element with a click
+            document.getElementById("appendItemButton").click();
+        }
+    })
+}
+
+function appendToList() {
+    let userInput = document.getElementById("userInputField").value;
+    if (userInput === "") { return } // Check for empty input
+    let listElement = document.createElement('li');
+    listElement.appendChild(document.createTextNode(userInput));
+    document.getElementById("list").appendChild(listElement);
+    document.getElementById("userInputField").value = "";
+}
+
+function removeListElement(element) {
+    element.parentNode.removeChild(element)
+}
+
+function highlight(element) {
+    document.getElementById("item1").style.backgroundColor = "lightgray";
+    document.getElementById("item2").style.backgroundColor = "lightgray";
+    document.getElementById("item3").style.backgroundColor = "lightgray";
+    document.getElementById("item4").style.backgroundColor = "lightgray";
+    element.style.backgroundColor = "orange";
+}
+
+checkForReturnKey()
